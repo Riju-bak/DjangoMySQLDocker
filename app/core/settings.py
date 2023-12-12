92afa42228b5424b4653d8c9129fd1d3f85cbd27
Riju-bak <defaltsith@gmail.com>
2023-12-12 15:40:18 -0500

    change lang en-us

--- app/core/settings.py
+++ app/core/settings.py
@@ -109,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'fr-fr'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Europe/Paris'
+TIME_ZONE = 'America/Toronto'
 
 USE_I18N = True
 
